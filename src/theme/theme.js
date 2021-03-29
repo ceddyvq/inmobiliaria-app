76b15530d3a5bfcf17712784e7c84dc5bd4471f6
ceddyvq <ceddyvq@gmail.com>
2021-03-29 13:20:29 -0500

    material design parte1

---
+++ src/theme/theme.js
@@ -0,0 +1,21 @@
+import {createMuiTheme} from '@material-ui/core/style';
+
+const theme = createMuiTheme({
+    typography :{
+        useNextVariants:true
+    },
+    palette :{
+        primary : {
+            main : '·10A75F'
+        },
+        common:{
+            white :'white'
+        },
+        secondary:{
+            main:'e53935'
+        }
+,
+spacing : 10    }
+});
+
+export default theme;
