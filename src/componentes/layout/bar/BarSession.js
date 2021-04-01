35bb00ecb7fa199d159bf80c951376baa0960d90
ceddyvq <ceddyvq@gmail.com>
2021-04-01 14:26:59 -0500

    navbar minuto 4 commit

---
+++ src/componentes/layout/bar/BarSession.js
@@ -0,0 +1,18 @@
+import { Toolbar, Typography } from '@material-ui/core';
+import React, { Component } from 'react';
+
+class BarSession extends Component {
+    render() {
+        return (
+            <div>
+                <Toolbar>
+                    <Typography variant="h6">
+                        vaxi homes
+                    </Typography>
+                </Toolbar> 
+            </div>
+        );
+    }
+}
+
+export default BarSession;
