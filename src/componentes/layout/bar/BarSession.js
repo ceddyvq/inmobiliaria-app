c420d24fc13f939ef2517560707d6e2334c98d45
ceddyvq <ceddyvq@gmail.com>
2021-04-02 12:24:22 -0500

    navbar parte 2

--- src/componentes/layout/bar/BarSession.js
+++ src/componentes/layout/bar/BarSession.js
@@ -1,18 +1,51 @@
-import { Toolbar, Typography } from '@material-ui/core';
+import { Button, IconButton, Toolbar, Typography } from '@material-ui/core';
 import React, { Component } from 'react';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles=theme => ({
+    sectionDesktop:{
+        display:"none",
+        [theme.breakpoints.up("md")]:{
+            display:"flex"
+        }
+    },
+    sectionMobile:{
+        display:"flex",
+        [theme.breakpoints.up("md")]:{
+            display:"none"
+        }
+    },
+    grow:{
+        flexGrow:1
+    }
+});
 
 class BarSession extends Component {
     render() {
+
+        const {classes}=this.props;
         return (
             <div>
                 <Toolbar>
+                    <IconButton coloe="inherit">
+                        <i className="material-icons">menu</i>
+                    </IconButton>
                     <Typography variant="h6">
                         vaxi homes
                     </Typography>
+                    <div className={classes.grow}></div>
+                    <div className={classes.sectionDesktop}>
+                        <Button color="inherit">Login</Button>
+                    </div>
+                    <div className={classes.sectionMobile}>
+                        <IconButton color="inherit">
+                            <i className="material-icons">more_vert</i>
+                        </IconButton>
+                    </div>
                 </Toolbar> 
             </div>
         );
     }
 }
 
-export default BarSession;
+export default withStyles(styles)(BarSession);
